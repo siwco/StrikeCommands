097f6b9ebed6a8f7fdc55f2ba523fe2974c57d46
siwoo <jamieyun06@gmail.com>
2024-10-20 23:22:14 +0900

    optimize code ig

--- src/main/java/club/siwoo/strikecommands/admin/ForceWin.java
+++ src/main/java/club/siwoo/strikecommands/admin/ForceWin.java
@@ -6,8 +6,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 
 public class ForceWin implements CommandExecutor {
 
