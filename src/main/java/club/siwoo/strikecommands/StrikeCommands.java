c04dca31324f81106160d66961a3b70adb057980
siwoo <jamieyun06@gmail.com>
2024-10-20 23:33:00 +0900

    add ocnfig file

--- src/main/java/club/siwoo/strikecommands/StrikeCommands.java
+++ src/main/java/club/siwoo/strikecommands/StrikeCommands.java
@@ -3,12 +3,19 @@ package club.siwoo.strikecommands;
 import club.siwoo.strikecommands.admin.ForceWin;
 import ga.strikepractice.StrikePractice;
 import ga.strikepractice.api.StrikePracticeAPI;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 public final class StrikeCommands extends JavaPlugin implements Listener {
 
     private StrikePracticeAPI api;
+    File newConfig;
+    FileConfiguration newConfigz;
 
     @Override
     public void onEnable() {
@@ -16,12 +23,26 @@ public final class StrikeCommands extends JavaPlugin implements Listener {
         registerCommands();
     }
 
+    public void registerCommands() {
+        getCommand("forcewin").setExecutor(new ForceWin());
+    }
+
+    public void checkConfig() {
+        newConfig = new File(getDataFolder(), "config.yml");
+        newConfigz = YamlConfiguration.loadConfiguration(newConfig);
+        saveNewConfig();
+    }
+
+    public void saveNewConfig() {
+        try {
+            newConfigz.save(newConfig);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void onDisable() {
         getLogger().info("Goodbye!");
-    }
-
-    public void registerCommands() {
-        getCommand("forcewin").setExecutor(new ForceWin());
     }
 }
