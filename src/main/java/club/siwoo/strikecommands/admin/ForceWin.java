2cbf2144de253b4fb7651ba3c04385769aa29c96
siwoo <jamieyun06@gmail.com>
2024-10-20 23:19:33 +0900

    forcewin command

--- src/main/java/club/siwoo/strikecommands/admin/ForceWin.java
+++ src/main/java/club/siwoo/strikecommands/admin/ForceWin.java
@@ -14,19 +14,31 @@ public class ForceWin implements CommandExecutor {
     private StrikePracticeAPI api;
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
-        if(sender.hasPermission("strikecommands.admin")) {
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        Player p = (Player) sender;
+
+        if(p.hasPermission("strikecommands.admin")) {
+            if(args.length == 0) {
+                api.forceWinFight(p);
+                p.sendMessage("Force Won The Fight!");
+            } else if(args.length == 1) {
+                // detect args type
+                if(args[1].equalsIgnoreCase("fight")) {
+                    api.forceWinFight(p);
+                    p.sendMessage("Force Won The Fight!");
+                } else if(args[1].equalsIgnoreCase("round")) {
+                    api.forceWinRound(p);
+                    p.sendMessage("Force Won The Round!");
+                } else {
+                    p.sendMessage("Invalid Option! Only Use (fight/round)");
+                }
+            } else {
+                p.sendMessage("Usage: /forcewin (fight/round)");
+            }
             return true;
         } else {
+            p.sendMessage("You don't have permission to use this command!");
             return true;
         }
-    }
-
-    public void forceWinFight(Player p) {
-        // Method
-    }
-
-    public void forceWinRound(Player p) {
-        // Method
     }
 }
